b7fdbb095911f63ecd81fe9011584bb1c9150bea
Christian Slanzi <christian.slanzi@gmail.com>
2020-07-21 19:52:49 +0200

    handle failure case

--- GitHubFollowers/GitHubFollowers/Managers/GitHubManager.swift
+++ GitHubFollowers/GitHubFollowers/Managers/GitHubManager.swift
@@ -26,16 +26,26 @@ class GitHubManager: GitHubNetworking {
     }
     
     func fetchFollowers(for username: String, page: Int, completion: @escaping (Result<[Follower], Error>) -> Void) {
-        subscription = gitHubService.fetchFollowers(for: username, page: 0).sink(receiveCompletion: { completion in
-           print(completion)
+        subscription = gitHubService.fetchFollowers(for: username, page: 0).sink(receiveCompletion: { subcompletion in
+            switch subcompletion {
+            case .finished:
+                break
+            case .failure(let error):
+                completion(.failure(error))
+            }
         }) { (followers) in
             completion(.success(followers))
         }
     }
     
     func fetchUserInfo(for username: String, completion: @escaping (Result<User, Error>) -> Void) {
-        subscription = gitHubService.fetchUserInfo(for: username).sink(receiveCompletion: { completion in
-           print(completion)
+        subscription = gitHubService.fetchUserInfo(for: username).sink(receiveCompletion: { subcompletion in
+            switch subcompletion {
+            case .finished:
+                break
+            case .failure(let error):
+                completion(.failure(error))
+            }
         }) { (userInfo) in
             completion(.success(userInfo))
         }
