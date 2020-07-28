f3f4ffc99d30198d74b556627c3d4200056fa50f
Christian Slanzi <christian.slanzi@gmail.com>
2020-07-28 17:20:20 +0200

    setup basic dummy view layout

--- GitHubFollowers/GitHubFollowers/Scenes/UserProfile/Views/ProfileView.swift
+++ GitHubFollowers/GitHubFollowers/Scenes/UserProfile/Views/ProfileView.swift
@@ -10,21 +10,102 @@ import UIKit
 
 class ProfileView: UIView {
     
-    public init() {
-        super.init(frame: .zero)
+    // MARK: - Properties
+
+    let profileImageView = CachedImageView(frame: .zero)
+    let usernameLabel = UILabel()
+    let nameLabel = UILabel()
+    let locationImageView = UIImageView()
+    let locationLabel = UILabel()
+    let bioLabel = UILabel()
+    
+    public convenience init() {
+        self.init(frame: .zero)
         setupViews()
     }
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
+        setupCostraints()
     }
     
     required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setupViews() {
+    private func setupViews() {
         backgroundColor = .systemBackground
+        
+        let user = User(login: "user", avatarUrl: "http://avatarUrl", name: nil, location: nil, bio: nil, publicRepos: 0, publicGists: 0, htmlUrl: "", following: 0, followers: 0, createdAt: Date())
+        
+        profileImageView.load(url: URL(string: user.avatarUrl)!)
+
+        usernameLabel.text = user.login
+
+        nameLabel.text = user.name ?? "No Name"
+
+        locationLabel.text = user.location ?? "No Location"
+
+        bioLabel.text = user.bio ?? "No Bio Available"
+        bioLabel.numberOfLines = 3
+
+        //locationImageView.image = SFSymbols.location
+        locationImageView.tintColor = .secondaryLabel
+        
+        addSubview(profileImageView)
+        addSubview(usernameLabel)
+        addSubview(nameLabel)
+        addSubview(locationImageView)
+        addSubview(locationLabel)
+        addSubview(bioLabel)
     }
+    
+    private func setupCostraints() {
+        let padding: CGFloat = 20
+        let textImagePadding: CGFloat = 12
+
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        locationImageView.translatesAutoresizingMaskIntoConstraints = false
+        locationLabel.translatesAutoresizingMaskIntoConstraints = false
+        bioLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        
+        NSLayoutConstraint.activate([
+            profileImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: padding),
+            profileImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            profileImageView.widthAnchor.constraint(equalToConstant: 90),
+            profileImageView.heightAnchor.constraint(equalToConstant: 90),
+
+            usernameLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: textImagePadding),
+            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 38),
+
+            nameLabel.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: textImagePadding),
+            nameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            nameLabel.heightAnchor.constraint(equalToConstant: 20),
+
+            locationImageView.bottomAnchor.constraint(equalTo: profileImageView.bottomAnchor),
+            locationImageView.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor,
+                                                       constant: textImagePadding),
+            locationImageView.widthAnchor.constraint(equalToConstant: 20),
+            locationImageView.heightAnchor.constraint(equalToConstant: 20),
+
+            locationLabel.centerYAnchor.constraint(equalTo: locationImageView.centerYAnchor),
+            locationLabel.leadingAnchor.constraint(equalTo: locationImageView.trailingAnchor, constant: 5),
+            locationLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            locationLabel.heightAnchor.constraint(equalToConstant: 20),
+
+            bioLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: textImagePadding),
+            bioLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+            bioLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            bioLabel.heightAnchor.constraint(equalToConstant: 90)
+        ])
+
+    }
+    
 }
